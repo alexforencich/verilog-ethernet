bfe6c37ca93f856afa4ce32f536a41e3cc4b9c3c
Alex Forencich <alex@alexforencich.com>
2015-02-26 19:00:33 -0800

    Add ethernet FCS inserter and checker

---
+++ rtl/axis_eth_fcs_insert.v
@@ -0,0 +1,320 @@
+/*
+
+Copyright (c) 2015 Alex Forencich
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
+
+// Language: Verilog 2001
+
+`timescale 1ns / 1ps
+
+/*
+ * AXI4-Stream Ethernet FCS inserter
+ */
+module axis_eth_fcs_insert #
+(
+    parameter ENABLE_PADDING = 0,
+    parameter MIN_FRAME_LENGTH = 64
+)
+(
+    input  wire        clk,
+    input  wire        rst,
+    
+    /*
+     * AXI input
+     */
+    input  wire [7:0]  input_axis_tdata,
+    input  wire        input_axis_tvalid,
+    output wire        input_axis_tready,
+    input  wire        input_axis_tlast,
+    input  wire        input_axis_tuser,
+    
+    /*
+     * AXI output
+     */
+    output wire [7:0]  output_axis_tdata,
+    output wire        output_axis_tvalid,
+    input  wire        output_axis_tready,
+    output wire        output_axis_tlast,
+    output wire        output_axis_tuser,
+
+    /*
+     * Status
+     */
+    output wire        busy
+);
+
+localparam [1:0]
+    STATE_IDLE = 2'd0,
+    STATE_PAYLOAD = 2'd1,
+    STATE_PAD = 2'd2,
+    STATE_FCS = 2'd3;
+
+reg [1:0] state_reg = STATE_IDLE, state_next;
+
+// datapath control signals
+reg reset_crc;
+reg update_crc;
+
+reg [7:0] frame_ptr_reg = 0, frame_ptr_next;
+
+reg busy_reg = 0;
+
+reg input_axis_tready_reg = 0, input_axis_tready_next;
+
+reg [31:0] crc_state = 32'hFFFFFFFF;
+wire [31:0] crc_next;
+
+// internal datapath
+reg [7:0]  output_axis_tdata_int;
+reg        output_axis_tvalid_int;
+reg        output_axis_tready_int = 0;
+reg        output_axis_tlast_int;
+reg        output_axis_tuser_int;
+wire       output_axis_tready_int_early;
+
+assign input_axis_tready = input_axis_tready_reg;
+
+assign busy = busy_reg;
+
+eth_crc_8
+eth_crc_8_inst (
+    .data_in(output_axis_tdata_int),
+    .crc_state(crc_state),
+    .crc_next(crc_next)
+);
+
+always @* begin
+    state_next = STATE_IDLE;
+
+    reset_crc = 0;
+    update_crc = 0;
+
+    frame_ptr_next = frame_ptr_reg;
+
+    input_axis_tready_next = 0;
+
+    output_axis_tdata_int = 0;
+    output_axis_tvalid_int = 0;
+    output_axis_tlast_int = 0;
+    output_axis_tuser_int = 0;
+
+    case (state_reg)
+        STATE_IDLE: begin
+            // idle state - wait for data
+            input_axis_tready_next = output_axis_tready_int_early;
+            frame_ptr_next = 0;
+            reset_crc = 1;
+
+            output_axis_tdata_int = input_axis_tdata;
+            output_axis_tvalid_int = input_axis_tvalid;
+            output_axis_tlast_int = 0;
+            output_axis_tuser_int = 0;
+
+            if (input_axis_tready & input_axis_tvalid) begin
+                frame_ptr_next = 1;
+                reset_crc = 0;
+                update_crc = 1;
+                state_next = STATE_PAYLOAD;
+            end else begin
+                state_next = STATE_IDLE;
+            end
+        end
+        STATE_PAYLOAD: begin
+            // transfer payload
+            input_axis_tready_next = output_axis_tready_int_early;
+
+            output_axis_tdata_int = input_axis_tdata;
+            output_axis_tvalid_int = input_axis_tvalid;
+            output_axis_tlast_int = 0;
+            output_axis_tuser_int = 0;
+
+            if (input_axis_tready & input_axis_tvalid) begin
+                frame_ptr_next = frame_ptr_reg + 1;
+                update_crc = 1;
+                if (input_axis_tlast) begin
+                    if (input_axis_tuser) begin
+                        output_axis_tlast_int = 1;
+                        output_axis_tuser_int = 1;
+                        reset_crc = 1;
+                        state_next = STATE_IDLE;
+                    end else begin
+                        input_axis_tready_next = 0;
+                        if (ENABLE_PADDING && frame_ptr_next < MIN_FRAME_LENGTH-5) begin
+                            state_next = STATE_PAD;
+                        end else begin
+                            frame_ptr_next = 0;
+                            state_next = STATE_FCS;
+                        end
+                    end
+                end else begin
+                    state_next = STATE_PAYLOAD;
+                end
+            end else begin
+                state_next = STATE_PAYLOAD;
+            end
+        end
+        STATE_PAD: begin
+            // insert padding
+            input_axis_tready_next = 0;
+
+            output_axis_tdata_int = 0;
+            output_axis_tvalid_int = 1;
+            output_axis_tlast_int = 0;
+            output_axis_tuser_int = 0;
+
+            if (output_axis_tready_int) begin
+                frame_ptr_next = frame_ptr_reg + 1;
+                update_crc = 1;
+                if (frame_ptr_next < MIN_FRAME_LENGTH-5) begin
+                    state_next = STATE_PAD;
+                end else begin
+                    frame_ptr_next = 0;
+                    state_next = STATE_FCS;
+                end
+            end
+        end
+        STATE_FCS: begin
+            // send FCS
+            input_axis_tready_next = 0;
+
+            case (frame_ptr_reg)
+                2'd0: output_axis_tdata_int = ~crc_state[7:0];
+                2'd1: output_axis_tdata_int = ~crc_state[15:8];
+                2'd2: output_axis_tdata_int = ~crc_state[23:16];
+                2'd3: output_axis_tdata_int = ~crc_state[31:24];
+            endcase
+            output_axis_tvalid_int = 1;
+            output_axis_tlast_int = 0;
+            output_axis_tuser_int = 0;
+
+            if (output_axis_tready_int) begin
+                frame_ptr_next = frame_ptr_reg + 1;
+
+                if (frame_ptr_reg < 3) begin
+                    state_next = STATE_FCS;
+                end else begin
+                    reset_crc = 1;
+                    frame_ptr_next = 0;
+                    output_axis_tlast_int = 1;
+                    input_axis_tready_next = output_axis_tready_int_early;
+                    state_next = STATE_IDLE;
+                end
+            end else begin
+                state_next = STATE_FCS;
+            end
+        end
+    endcase
+end
+
+always @(posedge clk or posedge rst) begin
+    if (rst) begin
+        state_reg <= STATE_IDLE;
+        
+        frame_ptr_reg <= 0;
+        
+        input_axis_tready_reg <= 0;
+
+        busy_reg <= 0;
+
+        crc_state <= 32'hFFFFFFFF;
+    end else begin
+        state_reg <= state_next;
+
+        frame_ptr_reg <= frame_ptr_next;
+
+        input_axis_tready_reg <= input_axis_tready_next;
+
+        busy_reg <= state_next != STATE_IDLE;
+
+        // datapath
+        if (reset_crc) begin
+            crc_state <= 32'hFFFFFFFF;
+        end else if (update_crc) begin
+            crc_state <= crc_next;
+        end
+    end
+end
+
+// output datapath logic
+reg [7:0]  output_axis_tdata_reg = 0;
+reg        output_axis_tvalid_reg = 0;
+reg        output_axis_tlast_reg = 0;
+reg        output_axis_tuser_reg = 0;
+
+reg [7:0]  temp_axis_tdata_reg = 0;
+reg        temp_axis_tvalid_reg = 0;
+reg        temp_axis_tlast_reg = 0;
+reg        temp_axis_tuser_reg = 0;
+
+assign output_axis_tdata = output_axis_tdata_reg;
+assign output_axis_tvalid = output_axis_tvalid_reg;
+assign output_axis_tlast = output_axis_tlast_reg;
+assign output_axis_tuser = output_axis_tuser_reg;
+
+// enable ready input next cycle if output is ready or if there is space in both output registers or if there is space in the temp register that will not be filled next cycle
+assign output_axis_tready_int_early = output_axis_tready | (~temp_axis_tvalid_reg & ~output_axis_tvalid_reg) | (~temp_axis_tvalid_reg & ~output_axis_tvalid_int);
+
+always @(posedge clk or posedge rst) begin
+    if (rst) begin
+        output_axis_tdata_reg <= 0;
+        output_axis_tvalid_reg <= 0;
+        output_axis_tlast_reg <= 0;
+        output_axis_tuser_reg <= 0;
+        output_axis_tready_int <= 0;
+        temp_axis_tdata_reg <= 0;
+        temp_axis_tvalid_reg <= 0;
+        temp_axis_tlast_reg <= 0;
+        temp_axis_tuser_reg <= 0;
+    end else begin
+        // transfer sink ready state to source
+        output_axis_tready_int <= output_axis_tready_int_early;
+
+        if (output_axis_tready_int) begin
+            // input is ready
+            if (output_axis_tready | ~output_axis_tvalid_reg) begin
+                // output is ready or currently not valid, transfer data to output
+                output_axis_tdata_reg <= output_axis_tdata_int;
+                output_axis_tvalid_reg <= output_axis_tvalid_int;
+                output_axis_tlast_reg <= output_axis_tlast_int;
+                output_axis_tuser_reg <= output_axis_tuser_int;
+            end else begin
+                // output is not ready and currently valid, store input in temp
+                temp_axis_tdata_reg <= output_axis_tdata_int;
+                temp_axis_tvalid_reg <= output_axis_tvalid_int;
+                temp_axis_tlast_reg <= output_axis_tlast_int;
+                temp_axis_tuser_reg <= output_axis_tuser_int;
+            end
+        end else if (output_axis_tready) begin
+            // input is not ready, but output is ready
+            output_axis_tdata_reg <= temp_axis_tdata_reg;
+            output_axis_tvalid_reg <= temp_axis_tvalid_reg;
+            output_axis_tlast_reg <= temp_axis_tlast_reg;
+            output_axis_tuser_reg <= temp_axis_tuser_reg;
+            temp_axis_tdata_reg <= 0;
+            temp_axis_tvalid_reg <= 0;
+            temp_axis_tlast_reg <= 0;
+            temp_axis_tuser_reg <= 0;
+        end
+    end
+end
+
+endmodule
---
+++ tb/test_axis_eth_fcs_check.py
@@ -0,0 +1,416 @@
+#!/usr/bin/env python2
+"""
+
+Copyright (c) 2015 Alex Forencich
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+"""
+
+from myhdl import *
+import os
+from Queue import Queue
+
+import axis_ep
+import eth_ep
+
+module = 'axis_eth_fcs_check'
+
+srcs = []
+
+srcs.append("../rtl/%s.v" % module)
+srcs.append("../rtl/eth_crc_8.v")
+srcs.append("test_%s.v" % module)
+
+src = ' '.join(srcs)
+
+build_cmd = "iverilog -o test_%s.vvp %s" % (module, src)
+
+def dut_axis_eth_fcs_check(clk,
+                           rst,
+                           current_test,
+
+                           input_axis_tdata,
+                           input_axis_tvalid,
+                           input_axis_tready,
+                           input_axis_tlast,
+                           input_axis_tuser,
+
+                           output_axis_tdata,
+                           output_axis_tvalid,
+                           output_axis_tready,
+                           output_axis_tlast,
+                           output_axis_tuser,
+
+                           busy,
+                           error_bad_fcs):
+
+    if os.system(build_cmd):
+        raise Exception("Error running build command")
+    return Cosimulation("vvp -m myhdl test_%s.vvp -lxt2" % module,
+                clk=clk,
+                rst=rst,
+                current_test=current_test,
+
+                input_axis_tdata=input_axis_tdata,
+                input_axis_tvalid=input_axis_tvalid,
+                input_axis_tready=input_axis_tready,
+                input_axis_tlast=input_axis_tlast,
+                input_axis_tuser=input_axis_tuser,
+
+                output_axis_tdata=output_axis_tdata,
+                output_axis_tvalid=output_axis_tvalid,
+                output_axis_tready=output_axis_tready,
+                output_axis_tlast=output_axis_tlast,
+                output_axis_tuser=output_axis_tuser,
+
+                busy=busy,
+                error_bad_fcs=error_bad_fcs)
+
+def bench():
+
+    # Parameters
+
+
+    # Inputs
+    clk = Signal(bool(0))
+    rst = Signal(bool(0))
+    current_test = Signal(intbv(0)[8:])
+
+    input_axis_tdata = Signal(intbv(0)[8:])
+    input_axis_tvalid = Signal(bool(0))
+    input_axis_tlast = Signal(bool(0))
+    input_axis_tuser = Signal(bool(0))
+    output_axis_tready = Signal(bool(0))
+
+    # Outputs
+    input_axis_tready = Signal(bool(0))
+    output_axis_tdata = Signal(intbv(0)[8:])
+    output_axis_tvalid = Signal(bool(0))
+    output_axis_tlast = Signal(bool(0))
+    output_axis_tuser = Signal(bool(0))
+    busy = Signal(bool(0))
+    error_bad_fcs = Signal(bool(0))
+
+    # sources and sinks
+    source_queue = Queue()
+    source_pause = Signal(bool(0))
+    sink_queue = Queue()
+    sink_pause = Signal(bool(0))
+
+    source = axis_ep.AXIStreamSource(clk,
+                                     rst,
+                                     tdata=input_axis_tdata,
+                                     tvalid=input_axis_tvalid,
+                                     tready=input_axis_tready,
+                                     tlast=input_axis_tlast,
+                                     tuser=input_axis_tuser,
+                                     fifo=source_queue,
+                                     pause=source_pause,
+                                     name='source')
+
+    sink = axis_ep.AXIStreamSink(clk,
+                                rst,
+                                tdata=output_axis_tdata,
+                                tvalid=output_axis_tvalid,
+                                tready=output_axis_tready,
+                                tlast=output_axis_tlast,
+                                tuser=output_axis_tuser,
+                                fifo=sink_queue,
+                                pause=sink_pause,
+                                name='sink')
+
+    # DUT
+    dut = dut_axis_eth_fcs_check(clk,
+                                 rst,
+                                 current_test,
+
+                                 input_axis_tdata,
+                                 input_axis_tvalid,
+                                 input_axis_tready,
+                                 input_axis_tlast,
+                                 input_axis_tuser,
+
+                                 output_axis_tdata,
+                                 output_axis_tvalid,
+                                 output_axis_tready,
+                                 output_axis_tlast,
+                                 output_axis_tuser,
+
+                                 busy,
+                                 error_bad_fcs)
+
+    @always(delay(4))
+    def clkgen():
+        clk.next = not clk
+
+    def wait_normal():
+        while input_axis_tvalid or output_axis_tvalid:
+            yield clk.posedge
+
+    def wait_pause_source():
+        while input_axis_tvalid or output_axis_tvalid:
+            source_pause.next = True
+            yield clk.posedge
+            yield clk.posedge
+            yield clk.posedge
+            source_pause.next = False
+            yield clk.posedge
+
+    def wait_pause_sink():
+        while input_axis_tvalid or output_axis_tvalid:
+            sink_pause.next = True
+            yield clk.posedge
+            yield clk.posedge
+            yield clk.posedge
+            sink_pause.next = False
+            yield clk.posedge
+
+    @instance
+    def check():
+        yield delay(100)
+        yield clk.posedge
+        rst.next = 1
+        yield clk.posedge
+        rst.next = 0
+        yield clk.posedge
+        yield delay(100)
+        yield clk.posedge
+
+        # testbench stimulus
+
+        for payload_len in list(range(1,18))+list(range(64,82)):
+            yield clk.posedge
+            print("test 1: test packet, length %d" % payload_len)
+            current_test.next = 1
+
+            test_frame = eth_ep.EthFrame()
+            test_frame.eth_dest_mac = 0xDAD1D2D3D4D5
+            test_frame.eth_src_mac = 0x5A5152535455
+            test_frame.eth_type = 0x8000
+            test_frame.payload = bytearray(range(payload_len))
+            test_frame.update_fcs()
+
+            axis_frame = test_frame.build_axis_fcs()
+
+            for wait in wait_normal, wait_pause_source, wait_pause_sink:
+                source_queue.put(axis_frame)
+                yield clk.posedge
+                yield clk.posedge
+
+                yield wait()
+
+                yield clk.posedge
+                yield clk.posedge
+                yield clk.posedge
+
+                rx_frame = None
+                if not sink_queue.empty():
+                    rx_frame = sink_queue.get()
+
+                eth_frame = eth_ep.EthFrame()
+                eth_frame.parse_axis(rx_frame)
+                eth_frame.update_fcs()
+
+                assert eth_frame == test_frame
+                assert not rx_frame.user[-1]
+
+                assert sink_queue.empty()
+
+                yield delay(100)
+
+            yield clk.posedge
+            print("test 2: back-to-back packets, length %d" % payload_len)
+            current_test.next = 2
+
+            test_frame1 = eth_ep.EthFrame()
+            test_frame1.eth_dest_mac = 0xDAD1D2D3D4D5
+            test_frame1.eth_src_mac = 0x5A5152535455
+            test_frame1.eth_type = 0x8000
+            test_frame1.payload = bytearray(range(payload_len))
+            test_frame1.update_fcs()
+            test_frame2 = eth_ep.EthFrame()
+            test_frame2.eth_dest_mac = 0xDAD1D2D3D4D5
+            test_frame2.eth_src_mac = 0x5A5152535455
+            test_frame2.eth_type = 0x8000
+            test_frame2.payload = bytearray(range(payload_len))
+            test_frame2.update_fcs()
+
+            axis_frame1 = test_frame1.build_axis_fcs()
+            axis_frame2 = test_frame2.build_axis_fcs()
+
+            for wait in wait_normal, wait_pause_source, wait_pause_sink:
+                source_queue.put(axis_frame1)
+                source_queue.put(axis_frame2)
+                yield clk.posedge
+                yield clk.posedge
+
+                yield wait()
+
+                yield clk.posedge
+                yield clk.posedge
+                yield clk.posedge
+
+                rx_frame = None
+                if not sink_queue.empty():
+                    rx_frame = sink_queue.get()
+
+                eth_frame = eth_ep.EthFrame()
+                eth_frame.parse_axis(rx_frame)
+                eth_frame.update_fcs()
+
+                assert eth_frame == test_frame1
+                assert not rx_frame.user[-1]
+
+                rx_frame = None
+                if not sink_queue.empty():
+                    rx_frame = sink_queue.get()
+
+                eth_frame = eth_ep.EthFrame()
+                eth_frame.parse_axis(rx_frame)
+                eth_frame.update_fcs()
+
+                assert eth_frame == test_frame2
+                assert not rx_frame.user[-1]
+
+                assert sink_queue.empty()
+
+                yield delay(100)
+
+            yield clk.posedge
+            print("test 3: tuser assert, length %d" % payload_len)
+            current_test.next = 3
+
+            test_frame1 = eth_ep.EthFrame()
+            test_frame1.eth_dest_mac = 0xDAD1D2D3D4D5
+            test_frame1.eth_src_mac = 0x5A5152535455
+            test_frame1.eth_type = 0x8000
+            test_frame1.payload = bytearray(range(payload_len))
+            test_frame1.update_fcs()
+            test_frame2 = eth_ep.EthFrame()
+            test_frame2.eth_dest_mac = 0xDAD1D2D3D4D5
+            test_frame2.eth_src_mac = 0x5A5152535455
+            test_frame2.eth_type = 0x8000
+            test_frame2.payload = bytearray(range(payload_len))
+            test_frame2.update_fcs()
+
+            axis_frame1 = test_frame1.build_axis_fcs()
+            axis_frame2 = test_frame2.build_axis_fcs()
+
+            axis_frame1.user = 1
+
+            for wait in wait_normal, wait_pause_source, wait_pause_sink:
+                source_queue.put(axis_frame1)
+                source_queue.put(axis_frame2)
+                yield clk.posedge
+                yield clk.posedge
+
+                yield wait()
+
+                yield clk.posedge
+                yield clk.posedge
+                yield clk.posedge
+
+                rx_frame = None
+                if not sink_queue.empty():
+                    rx_frame = sink_queue.get()
+
+                assert rx_frame.user[-1]
+
+                rx_frame = None
+                if not sink_queue.empty():
+                    rx_frame = sink_queue.get()
+
+                eth_frame = eth_ep.EthFrame()
+                eth_frame.parse_axis(rx_frame)
+                eth_frame.update_fcs()
+
+                assert eth_frame == test_frame2
+                assert not rx_frame.user[-1]
+
+                assert sink_queue.empty()
+
+                yield delay(100)
+
+            yield clk.posedge
+            print("test 4: bad FCS, length %d" % payload_len)
+            current_test.next = 4
+
+            test_frame1 = eth_ep.EthFrame()
+            test_frame1.eth_dest_mac = 0xDAD1D2D3D4D5
+            test_frame1.eth_src_mac = 0x5A5152535455
+            test_frame1.eth_type = 0x8000
+            test_frame1.payload = bytearray(range(payload_len))
+            test_frame1.update_fcs()
+            test_frame2 = eth_ep.EthFrame()
+            test_frame2.eth_dest_mac = 0xDAD1D2D3D4D5
+            test_frame2.eth_src_mac = 0x5A5152535455
+            test_frame2.eth_type = 0x8000
+            test_frame2.payload = bytearray(range(payload_len))
+            test_frame2.update_fcs()
+
+            axis_frame1 = test_frame1.build_axis_fcs()
+            axis_frame2 = test_frame2.build_axis_fcs()
+
+            axis_frame1.data[-1] ^= 0xff
+
+            for wait in wait_normal, wait_pause_source, wait_pause_sink:
+                source_queue.put(axis_frame1)
+                source_queue.put(axis_frame2)
+                yield clk.posedge
+                yield clk.posedge
+
+                yield wait()
+
+                yield clk.posedge
+                yield clk.posedge
+                yield clk.posedge
+
+                rx_frame = None
+                if not sink_queue.empty():
+                    rx_frame = sink_queue.get()
+
+                assert rx_frame.user[-1]
+
+                rx_frame = None
+                if not sink_queue.empty():
+                    rx_frame = sink_queue.get()
+
+                eth_frame = eth_ep.EthFrame()
+                eth_frame.parse_axis(rx_frame)
+                eth_frame.update_fcs()
+
+                assert eth_frame == test_frame2
+                assert not rx_frame.user[-1]
+
+                assert sink_queue.empty()
+
+                yield delay(100)
+
+        raise StopSimulation
+
+    return dut, source, sink, clkgen, check
+
+def test_bench():
+    sim = Simulation(bench())
+    sim.run()
+
+if __name__ == '__main__':
+    print("Running test...")
+    test_bench()
