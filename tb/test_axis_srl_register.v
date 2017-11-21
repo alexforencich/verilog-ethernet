4ef4ef262239633ef2ae30565e8d8ad47eea5235
Alex Forencich <alex@alexforencich.com>
2017-11-20 21:34:25 -0800

    Consolidate, add configuration parameters, and add tid and tdest ports to AXI stream SRL register

--- tb/test_axis_srl_register.v
+++ tb/test_axis_srl_register.v
@@ -33,6 +33,15 @@ module test_axis_srl_register;
 
 // Parameters
 parameter DATA_WIDTH = 8;
+parameter KEEP_ENABLE = (DATA_WIDTH>8);
+parameter KEEP_WIDTH = (DATA_WIDTH/8);
+parameter LAST_ENABLE = 1;
+parameter ID_ENABLE = 1;
+parameter ID_WIDTH = 8;
+parameter DEST_ENABLE = 1;
+parameter DEST_WIDTH = 8;
+parameter USER_ENABLE = 1;
+parameter USER_WIDTH = 1;
 
 // Inputs
 reg clk = 0;
@@ -40,17 +49,23 @@ reg rst = 0;
 reg [7:0] current_test = 0;
 
 reg [DATA_WIDTH-1:0] input_axis_tdata = 0;
+reg [KEEP_WIDTH-1:0] input_axis_tkeep = 0;
 reg input_axis_tvalid = 0;
 reg input_axis_tlast = 0;
-reg input_axis_tuser = 0;
+reg [ID_WIDTH-1:0] input_axis_tid = 0;
+reg [DEST_WIDTH-1:0] input_axis_tdest = 0;
+reg [USER_WIDTH-1:0] input_axis_tuser = 0;
 reg output_axis_tready = 0;
 
 // Outputs
 wire input_axis_tready;
 wire [DATA_WIDTH-1:0] output_axis_tdata;
+wire [KEEP_WIDTH-1:0] output_axis_tkeep;
 wire output_axis_tvalid;
 wire output_axis_tlast;
-wire output_axis_tuser;
+wire [ID_WIDTH-1:0] output_axis_tid;
+wire [DEST_WIDTH-1:0] output_axis_tdest;
+wire [USER_WIDTH-1:0] output_axis_tuser;
 
 initial begin
     // myhdl integration
@@ -59,16 +74,22 @@ initial begin
         rst,
         current_test,
         input_axis_tdata,
+        input_axis_tkeep,
         input_axis_tvalid,
         input_axis_tlast,
+        input_axis_tid,
+        input_axis_tdest,
         input_axis_tuser,
         output_axis_tready
     );
     $to_myhdl(
         input_axis_tready,
         output_axis_tdata,
+        output_axis_tkeep,
         output_axis_tvalid,
         output_axis_tlast,
+        output_axis_tid,
+        output_axis_tdest,
         output_axis_tuser
     );
 
@@ -78,22 +99,37 @@ initial begin
 end
 
 axis_srl_register #(
-    .DATA_WIDTH(DATA_WIDTH)
+    .DATA_WIDTH(DATA_WIDTH),
+    .KEEP_ENABLE(KEEP_ENABLE),
+    .KEEP_WIDTH(KEEP_WIDTH),
+    .LAST_ENABLE(LAST_ENABLE),
+    .ID_ENABLE(ID_ENABLE),
+    .ID_WIDTH(ID_WIDTH),
+    .DEST_ENABLE(DEST_ENABLE),
+    .DEST_WIDTH(DEST_WIDTH),
+    .USER_ENABLE(USER_ENABLE),
+    .USER_WIDTH(USER_WIDTH)
 )
 UUT (
     .clk(clk),
     .rst(rst),
-    // axi input
+    // AXI input
     .input_axis_tdata(input_axis_tdata),
+    .input_axis_tkeep(input_axis_tkeep),
     .input_axis_tvalid(input_axis_tvalid),
     .input_axis_tready(input_axis_tready),
     .input_axis_tlast(input_axis_tlast),
+    .input_axis_tid(input_axis_tid),
+    .input_axis_tdest(input_axis_tdest),
     .input_axis_tuser(input_axis_tuser),
-    // axi output
+    // AXI output
     .output_axis_tdata(output_axis_tdata),
+    .output_axis_tkeep(output_axis_tkeep),
     .output_axis_tvalid(output_axis_tvalid),
     .output_axis_tready(output_axis_tready),
     .output_axis_tlast(output_axis_tlast),
+    .output_axis_tid(output_axis_tid),
+    .output_axis_tdest(output_axis_tdest),
     .output_axis_tuser(output_axis_tuser)
 );
 
