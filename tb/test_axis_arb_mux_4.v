940c1210c18c6811b339d95484ae7d676eb7a663
Alex Forencich <alex@alexforencich.com>
2018-10-24 13:49:17 -0700

    Convert arbitrated mux to verilog parametrized arbitrated mux

--- tb/test_axis_arb_mux_4.v
+++ tb/test_axis_arb_mux_4.v
@@ -27,11 +27,12 @@ THE SOFTWARE.
 `timescale 1ns / 1ps
 
 /*
- * Testbench for axis_arb_mux_4
+ * Testbench for axis_arb_mux
  */
 module test_axis_arb_mux_4;
 
 // Parameters
+parameter S_COUNT = 4;
 parameter DATA_WIDTH = 8;
 parameter KEEP_ENABLE = (DATA_WIDTH>8);
 parameter KEEP_WIDTH = (DATA_WIDTH/8);
@@ -47,50 +48,26 @@ reg clk = 0;
 reg rst = 0;
 reg [7:0] current_test = 0;
 
-reg [DATA_WIDTH-1:0] input_0_axis_tdata = 0;
-reg [KEEP_WIDTH-1:0] input_0_axis_tkeep = 0;
-reg input_0_axis_tvalid = 0;
-reg input_0_axis_tlast = 0;
-reg [ID_WIDTH-1:0] input_0_axis_tid = 0;
-reg [DEST_WIDTH-1:0] input_0_axis_tdest = 0;
-reg [USER_WIDTH-1:0] input_0_axis_tuser = 0;
-reg [DATA_WIDTH-1:0] input_1_axis_tdata = 0;
-reg [KEEP_WIDTH-1:0] input_1_axis_tkeep = 0;
-reg input_1_axis_tvalid = 0;
-reg input_1_axis_tlast = 0;
-reg [ID_WIDTH-1:0] input_1_axis_tid = 0;
-reg [DEST_WIDTH-1:0] input_1_axis_tdest = 0;
-reg [USER_WIDTH-1:0] input_1_axis_tuser = 0;
-reg [DATA_WIDTH-1:0] input_2_axis_tdata = 0;
-reg [KEEP_WIDTH-1:0] input_2_axis_tkeep = 0;
-reg input_2_axis_tvalid = 0;
-reg input_2_axis_tlast = 0;
-reg [ID_WIDTH-1:0] input_2_axis_tid = 0;
-reg [DEST_WIDTH-1:0] input_2_axis_tdest = 0;
-reg [USER_WIDTH-1:0] input_2_axis_tuser = 0;
-reg [DATA_WIDTH-1:0] input_3_axis_tdata = 0;
-reg [KEEP_WIDTH-1:0] input_3_axis_tkeep = 0;
-reg input_3_axis_tvalid = 0;
-reg input_3_axis_tlast = 0;
-reg [ID_WIDTH-1:0] input_3_axis_tid = 0;
-reg [DEST_WIDTH-1:0] input_3_axis_tdest = 0;
-reg [USER_WIDTH-1:0] input_3_axis_tuser = 0;
+reg [S_COUNT*DATA_WIDTH-1:0] s_axis_tdata = 0;
+reg [S_COUNT*KEEP_WIDTH-1:0] s_axis_tkeep = 0;
+reg [S_COUNT-1:0] s_axis_tvalid = 0;
+reg [S_COUNT-1:0] s_axis_tlast = 0;
+reg [S_COUNT*ID_WIDTH-1:0] s_axis_tid = 0;
+reg [S_COUNT*DEST_WIDTH-1:0] s_axis_tdest = 0;
+reg [S_COUNT*USER_WIDTH-1:0] s_axis_tuser = 0;
 
-reg output_axis_tready = 0;
+reg m_axis_tready = 0;
 
 // Outputs
-wire input_0_axis_tready;
-wire input_1_axis_tready;
-wire input_2_axis_tready;
-wire input_3_axis_tready;
+wire [S_COUNT-1:0] s_axis_tready;
 
-wire [DATA_WIDTH-1:0] output_axis_tdata;
-wire [KEEP_WIDTH-1:0] output_axis_tkeep;
-wire output_axis_tvalid;
-wire output_axis_tlast;
-wire [ID_WIDTH-1:0] output_axis_tid;
-wire [DEST_WIDTH-1:0] output_axis_tdest;
-wire [USER_WIDTH-1:0] output_axis_tuser;
+wire [DATA_WIDTH-1:0] m_axis_tdata;
+wire [KEEP_WIDTH-1:0] m_axis_tkeep;
+wire m_axis_tvalid;
+wire m_axis_tlast;
+wire [ID_WIDTH-1:0] m_axis_tid;
+wire [DEST_WIDTH-1:0] m_axis_tdest;
+wire [USER_WIDTH-1:0] m_axis_tuser;
 
 initial begin
     // myhdl integration
@@ -98,48 +75,24 @@ initial begin
         clk,
         rst,
         current_test,
-        input_0_axis_tdata,
-        input_0_axis_tkeep,
-        input_0_axis_tvalid,
-        input_0_axis_tlast,
-        input_0_axis_tid,
-        input_0_axis_tdest,
-        input_0_axis_tuser,
-        input_1_axis_tdata,
-        input_1_axis_tkeep,
-        input_1_axis_tvalid,
-        input_1_axis_tlast,
-        input_1_axis_tid,
-        input_1_axis_tdest,
-        input_1_axis_tuser,
-        input_2_axis_tdata,
-        input_2_axis_tkeep,
-        input_2_axis_tvalid,
-        input_2_axis_tlast,
-        input_2_axis_tid,
-        input_2_axis_tdest,
-        input_2_axis_tuser,
-        input_3_axis_tdata,
-        input_3_axis_tkeep,
-        input_3_axis_tvalid,
-        input_3_axis_tlast,
-        input_3_axis_tid,
-        input_3_axis_tdest,
-        input_3_axis_tuser,
-        output_axis_tready
+        s_axis_tdata,
+        s_axis_tkeep,
+        s_axis_tvalid,
+        s_axis_tlast,
+        s_axis_tid,
+        s_axis_tdest,
+        s_axis_tuser,
+        m_axis_tready
     );
     $to_myhdl(
-        input_0_axis_tready,
-        input_1_axis_tready,
-        input_2_axis_tready,
-        input_3_axis_tready,
-        output_axis_tdata,
-        output_axis_tkeep,
-        output_axis_tvalid,
-        output_axis_tlast,
-        output_axis_tid,
-        output_axis_tdest,
-        output_axis_tuser
+        s_axis_tready,
+        m_axis_tdata,
+        m_axis_tkeep,
+        m_axis_tvalid,
+        m_axis_tlast,
+        m_axis_tid,
+        m_axis_tdest,
+        m_axis_tuser
     );
 
     // dump file
@@ -147,7 +100,8 @@ initial begin
     $dumpvars(0, test_axis_arb_mux_4);
 end
 
-axis_arb_mux_4 #(
+axis_arb_mux #(
+    .S_COUNT(S_COUNT),
     .DATA_WIDTH(DATA_WIDTH),
     .KEEP_ENABLE(KEEP_ENABLE),
     .KEEP_WIDTH(KEEP_WIDTH),
@@ -162,47 +116,23 @@ UUT (
     .clk(clk),
     .rst(rst),
     // AXI inputs
-    .input_0_axis_tdata(input_0_axis_tdata),
-    .input_0_axis_tkeep(input_0_axis_tkeep),
-    .input_0_axis_tvalid(input_0_axis_tvalid),
-    .input_0_axis_tready(input_0_axis_tready),
-    .input_0_axis_tlast(input_0_axis_tlast),
-    .input_0_axis_tid(input_0_axis_tid),
-    .input_0_axis_tdest(input_0_axis_tdest),
-    .input_0_axis_tuser(input_0_axis_tuser),
-    .input_1_axis_tdata(input_1_axis_tdata),
-    .input_1_axis_tkeep(input_1_axis_tkeep),
-    .input_1_axis_tvalid(input_1_axis_tvalid),
-    .input_1_axis_tready(input_1_axis_tready),
-    .input_1_axis_tlast(input_1_axis_tlast),
-    .input_1_axis_tid(input_1_axis_tid),
-    .input_1_axis_tdest(input_1_axis_tdest),
-    .input_1_axis_tuser(input_1_axis_tuser),
-    .input_2_axis_tdata(input_2_axis_tdata),
-    .input_2_axis_tkeep(input_2_axis_tkeep),
-    .input_2_axis_tvalid(input_2_axis_tvalid),
-    .input_2_axis_tready(input_2_axis_tready),
-    .input_2_axis_tlast(input_2_axis_tlast),
-    .input_2_axis_tid(input_2_axis_tid),
-    .input_2_axis_tdest(input_2_axis_tdest),
-    .input_2_axis_tuser(input_2_axis_tuser),
-    .input_3_axis_tdata(input_3_axis_tdata),
-    .input_3_axis_tkeep(input_3_axis_tkeep),
-    .input_3_axis_tvalid(input_3_axis_tvalid),
-    .input_3_axis_tready(input_3_axis_tready),
-    .input_3_axis_tlast(input_3_axis_tlast),
-    .input_3_axis_tid(input_3_axis_tid),
-    .input_3_axis_tdest(input_3_axis_tdest),
-    .input_3_axis_tuser(input_3_axis_tuser),
+    .s_axis_tdata(s_axis_tdata),
+    .s_axis_tkeep(s_axis_tkeep),
+    .s_axis_tvalid(s_axis_tvalid),
+    .s_axis_tready(s_axis_tready),
+    .s_axis_tlast(s_axis_tlast),
+    .s_axis_tid(s_axis_tid),
+    .s_axis_tdest(s_axis_tdest),
+    .s_axis_tuser(s_axis_tuser),
     // AXI output
-    .output_axis_tdata(output_axis_tdata),
-    .output_axis_tkeep(output_axis_tkeep),
-    .output_axis_tvalid(output_axis_tvalid),
-    .output_axis_tready(output_axis_tready),
-    .output_axis_tlast(output_axis_tlast),
-    .output_axis_tid(output_axis_tid),
-    .output_axis_tdest(output_axis_tdest),
-    .output_axis_tuser(output_axis_tuser)
+    .m_axis_tdata(m_axis_tdata),
+    .m_axis_tkeep(m_axis_tkeep),
+    .m_axis_tvalid(m_axis_tvalid),
+    .m_axis_tready(m_axis_tready),
+    .m_axis_tlast(m_axis_tlast),
+    .m_axis_tid(m_axis_tid),
+    .m_axis_tdest(m_axis_tdest),
+    .m_axis_tuser(m_axis_tuser)
 );
 
 endmodule
