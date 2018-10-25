9d813226d0e17c2737f542ec0ca181e0c03b5907
Alex Forencich <alex@alexforencich.com>
2018-10-24 22:16:05 -0700

    Convert generated demux to verilog parametrized demux

--- tb/test_axis_demux_4_64.py
+++ tb/test_axis_demux_4_64.py
@@ -27,9 +27,10 @@ from myhdl import *
 import os
 
 import axis_ep
+import math
 
-module = 'axis_demux_4'
-testbench = 'test_%s_64' % module
+module = 'axis_demux'
+testbench = 'test_%s_4_64' % module
 
 srcs = []
 
@@ -43,6 +44,7 @@ build_cmd = "iverilog -o %s.vvp %s" % (testbench, src)
 def bench():
 
     # Parameters
+    M_COUNT = 4
     DATA_WIDTH = 64
     KEEP_ENABLE = (DATA_WIDTH>8)
     KEEP_WIDTH = (DATA_WIDTH/8)
@@ -58,145 +60,85 @@ def bench():
     rst = Signal(bool(0))
     current_test = Signal(intbv(0)[8:])
 
-    input_axis_tdata = Signal(intbv(0)[DATA_WIDTH:])
-    input_axis_tkeep = Signal(intbv(1)[KEEP_WIDTH:])
-    input_axis_tvalid = Signal(bool(0))
-    input_axis_tlast = Signal(bool(0))
-    input_axis_tid = Signal(intbv(0)[ID_WIDTH:])
-    input_axis_tdest = Signal(intbv(0)[DEST_WIDTH:])
-    input_axis_tuser = Signal(intbv(0)[USER_WIDTH:])
+    s_axis_tdata = Signal(intbv(0)[DATA_WIDTH:])
+    s_axis_tkeep = Signal(intbv(1)[KEEP_WIDTH:])
+    s_axis_tvalid = Signal(bool(0))
+    s_axis_tlast = Signal(bool(0))
+    s_axis_tid = Signal(intbv(0)[ID_WIDTH:])
+    s_axis_tdest = Signal(intbv(0)[DEST_WIDTH:])
+    s_axis_tuser = Signal(intbv(0)[USER_WIDTH:])
 
-    output_0_axis_tready = Signal(bool(0))
-    output_1_axis_tready = Signal(bool(0))
-    output_2_axis_tready = Signal(bool(0))
-    output_3_axis_tready = Signal(bool(0))
+    m_axis_tready_list = [Signal(bool(0)) for i in range(M_COUNT)]
+
+    m_axis_tready = ConcatSignal(*reversed(m_axis_tready_list))
 
     enable = Signal(bool(0))
-    select = Signal(intbv(0)[2:])
+    drop = Signal(bool(0))
+    select = Signal(intbv(0)[math.ceil(math.log(M_COUNT, 2)):])
 
     # Outputs
-    input_axis_tready = Signal(bool(0))
+    s_axis_tready = Signal(bool(0))
 
-    output_0_axis_tdata = Signal(intbv(0)[DATA_WIDTH:])
-    output_0_axis_tkeep = Signal(intbv(1)[KEEP_WIDTH:])
-    output_0_axis_tvalid = Signal(bool(0))
-    output_0_axis_tlast = Signal(bool(0))
-    output_0_axis_tid = Signal(intbv(0)[ID_WIDTH:])
-    output_0_axis_tdest = Signal(intbv(0)[DEST_WIDTH:])
-    output_0_axis_tuser = Signal(intbv(0)[USER_WIDTH:])
-    output_1_axis_tdata = Signal(intbv(0)[DATA_WIDTH:])
-    output_1_axis_tkeep = Signal(intbv(1)[KEEP_WIDTH:])
-    output_1_axis_tvalid = Signal(bool(0))
-    output_1_axis_tlast = Signal(bool(0))
-    output_1_axis_tid = Signal(intbv(0)[ID_WIDTH:])
-    output_1_axis_tdest = Signal(intbv(0)[DEST_WIDTH:])
-    output_1_axis_tuser = Signal(intbv(0)[USER_WIDTH:])
-    output_2_axis_tdata = Signal(intbv(0)[DATA_WIDTH:])
-    output_2_axis_tkeep = Signal(intbv(1)[KEEP_WIDTH:])
-    output_2_axis_tvalid = Signal(bool(0))
-    output_2_axis_tlast = Signal(bool(0))
-    output_2_axis_tid = Signal(intbv(0)[ID_WIDTH:])
-    output_2_axis_tdest = Signal(intbv(0)[DEST_WIDTH:])
-    output_2_axis_tuser = Signal(intbv(0)[USER_WIDTH:])
-    output_3_axis_tdata = Signal(intbv(0)[DATA_WIDTH:])
-    output_3_axis_tkeep = Signal(intbv(1)[KEEP_WIDTH:])
-    output_3_axis_tvalid = Signal(bool(0))
-    output_3_axis_tlast = Signal(bool(0))
-    output_3_axis_tid = Signal(intbv(0)[ID_WIDTH:])
-    output_3_axis_tdest = Signal(intbv(0)[DEST_WIDTH:])
-    output_3_axis_tuser = Signal(intbv(0)[USER_WIDTH:])
+    m_axis_tdata = Signal(intbv(0)[M_COUNT*DATA_WIDTH:])
+    m_axis_tkeep = Signal(intbv(0xf)[M_COUNT*KEEP_WIDTH:])
+    m_axis_tvalid = Signal(intbv(0)[M_COUNT:])
+    m_axis_tlast = Signal(intbv(0)[M_COUNT:])
+    m_axis_tid = Signal(intbv(0)[M_COUNT*ID_WIDTH:])
+    m_axis_tdest = Signal(intbv(0)[M_COUNT*DEST_WIDTH:])
+    m_axis_tuser = Signal(intbv(0)[M_COUNT*USER_WIDTH:])
+
+    m_axis_tdata_list = [m_axis_tdata((i+1)*DATA_WIDTH, i*DATA_WIDTH) for i in range(M_COUNT)]
+    m_axis_tkeep_list = [m_axis_tkeep((i+1)*KEEP_WIDTH, i*KEEP_WIDTH) for i in range(M_COUNT)]
+    m_axis_tvalid_list = [m_axis_tvalid(i) for i in range(M_COUNT)]
+    m_axis_tlast_list = [m_axis_tlast(i) for i in range(M_COUNT)]
+    m_axis_tid_list = [m_axis_tid((i+1)*ID_WIDTH, i*ID_WIDTH) for i in range(M_COUNT)]
+    m_axis_tdest_list = [m_axis_tdest((i+1)*DEST_WIDTH, i*DEST_WIDTH) for i in range(M_COUNT)]
+    m_axis_tuser_list = [m_axis_tuser((i+1)*USER_WIDTH, i*USER_WIDTH) for i in range(M_COUNT)]
 
     # sources and sinks
     source_pause = Signal(bool(0))
-    sink_0_pause = Signal(bool(0))
-    sink_1_pause = Signal(bool(0))
-    sink_2_pause = Signal(bool(0))
-    sink_3_pause = Signal(bool(0))
+    sink_pause_list = []
+    sink_list = []
+    sink_logic_list = []
 
     source = axis_ep.AXIStreamSource()
 
     source_logic = source.create_logic(
         clk,
         rst,
-        tdata=input_axis_tdata,
-        tkeep=input_axis_tkeep,
-        tvalid=input_axis_tvalid,
-        tready=input_axis_tready,
-        tlast=input_axis_tlast,
-        tid=input_axis_tid,
-        tdest=input_axis_tdest,
-        tuser=input_axis_tuser,
+        tdata=s_axis_tdata,
+        tkeep=s_axis_tkeep,
+        tvalid=s_axis_tvalid,
+        tready=s_axis_tready,
+        tlast=s_axis_tlast,
+        tid=s_axis_tid,
+        tdest=s_axis_tdest,
+        tuser=s_axis_tuser,
         pause=source_pause,
         name='source'
     )
 
-    sink_0 = axis_ep.AXIStreamSink()
+    for k in range(M_COUNT):
+        s = axis_ep.AXIStreamSink()
+        p = Signal(bool(0))
 
-    sink_0_logic = sink_0.create_logic(
-        clk,
-        rst,
-        tdata=output_0_axis_tdata,
-        tkeep=output_0_axis_tkeep,
-        tvalid=output_0_axis_tvalid,
-        tready=output_0_axis_tready,
-        tlast=output_0_axis_tlast,
-        tid=output_0_axis_tid,
-        tdest=output_0_axis_tdest,
-        tuser=output_0_axis_tuser,
-        pause=sink_0_pause,
-        name='sink_0'
-    )
+        sink_list.append(s)
+        sink_pause_list.append(p)
 
-    sink_1 = axis_ep.AXIStreamSink()
-
-    sink_1_logic = sink_1.create_logic(
-        clk,
-        rst,
-        tdata=output_1_axis_tdata,
-        tkeep=output_1_axis_tkeep,
-        tvalid=output_1_axis_tvalid,
-        tready=output_1_axis_tready,
-        tlast=output_1_axis_tlast,
-        tid=output_1_axis_tid,
-        tdest=output_1_axis_tdest,
-        tuser=output_1_axis_tuser,
-        pause=sink_1_pause,
-        name='sink_1'
-    )
-
-    sink_2 = axis_ep.AXIStreamSink()
-
-    sink_2_logic = sink_2.create_logic(
-        clk,
-        rst,
-        tdata=output_2_axis_tdata,
-        tkeep=output_2_axis_tkeep,
-        tvalid=output_2_axis_tvalid,
-        tready=output_2_axis_tready,
-        tlast=output_2_axis_tlast,
-        tid=output_2_axis_tid,
-        tdest=output_2_axis_tdest,
-        tuser=output_2_axis_tuser,
-        pause=sink_2_pause,
-        name='sink_2'
-    )
-
-    sink_3 = axis_ep.AXIStreamSink()
-
-    sink_3_logic = sink_3.create_logic(
-        clk,
-        rst,
-        tdata=output_3_axis_tdata,
-        tkeep=output_3_axis_tkeep,
-        tvalid=output_3_axis_tvalid,
-        tready=output_3_axis_tready,
-        tlast=output_3_axis_tlast,
-        tid=output_3_axis_tid,
-        tdest=output_3_axis_tdest,
-        tuser=output_3_axis_tuser,
-        pause=sink_3_pause,
-        name='sink_3'
-    )
+        sink_logic_list.append(s.create_logic(
+            clk,
+            rst,
+            tdata=m_axis_tdata_list[k],
+            tkeep=m_axis_tkeep_list[k],
+            tvalid=m_axis_tvalid_list[k],
+            tready=m_axis_tready_list[k],
+            tlast=m_axis_tlast_list[k],
+            tid=m_axis_tid_list[k],
+            tdest=m_axis_tdest_list[k],
+            tuser=m_axis_tuser_list[k],
+            pause=p,
+            name='sink_%d' % k
+        ))
 
     # DUT
     if os.system(build_cmd):
@@ -208,49 +150,26 @@ def bench():
         rst=rst,
         current_test=current_test,
 
-        input_axis_tdata=input_axis_tdata,
-        input_axis_tkeep=input_axis_tkeep,
-        input_axis_tvalid=input_axis_tvalid,
-        input_axis_tready=input_axis_tready,
-        input_axis_tlast=input_axis_tlast,
-        input_axis_tid=input_axis_tid,
-        input_axis_tdest=input_axis_tdest,
-        input_axis_tuser=input_axis_tuser,
+        s_axis_tdata=s_axis_tdata,
+        s_axis_tkeep=s_axis_tkeep,
+        s_axis_tvalid=s_axis_tvalid,
+        s_axis_tready=s_axis_tready,
+        s_axis_tlast=s_axis_tlast,
+        s_axis_tid=s_axis_tid,
+        s_axis_tdest=s_axis_tdest,
+        s_axis_tuser=s_axis_tuser,
 
-        output_0_axis_tdata=output_0_axis_tdata,
-        output_0_axis_tkeep=output_0_axis_tkeep,
-        output_0_axis_tvalid=output_0_axis_tvalid,
-        output_0_axis_tready=output_0_axis_tready,
-        output_0_axis_tlast=output_0_axis_tlast,
-        output_0_axis_tid=output_0_axis_tid,
-        output_0_axis_tdest=output_0_axis_tdest,
-        output_0_axis_tuser=output_0_axis_tuser,
-        output_1_axis_tdata=output_1_axis_tdata,
-        output_1_axis_tkeep=output_1_axis_tkeep,
-        output_1_axis_tvalid=output_1_axis_tvalid,
-        output_1_axis_tready=output_1_axis_tready,
-        output_1_axis_tlast=output_1_axis_tlast,
-        output_1_axis_tid=output_1_axis_tid,
-        output_1_axis_tdest=output_1_axis_tdest,
-        output_1_axis_tuser=output_1_axis_tuser,
-        output_2_axis_tdata=output_2_axis_tdata,
-        output_2_axis_tkeep=output_2_axis_tkeep,
-        output_2_axis_tvalid=output_2_axis_tvalid,
-        output_2_axis_tready=output_2_axis_tready,
-        output_2_axis_tlast=output_2_axis_tlast,
-        output_2_axis_tid=output_2_axis_tid,
-        output_2_axis_tdest=output_2_axis_tdest,
-        output_2_axis_tuser=output_2_axis_tuser,
-        output_3_axis_tdata=output_3_axis_tdata,
-        output_3_axis_tkeep=output_3_axis_tkeep,
-        output_3_axis_tvalid=output_3_axis_tvalid,
-        output_3_axis_tready=output_3_axis_tready,
-        output_3_axis_tlast=output_3_axis_tlast,
-        output_3_axis_tid=output_3_axis_tid,
-        output_3_axis_tdest=output_3_axis_tdest,
-        output_3_axis_tuser=output_3_axis_tuser,
+        m_axis_tdata=m_axis_tdata,
+        m_axis_tkeep=m_axis_tkeep,
+        m_axis_tvalid=m_axis_tvalid,
+        m_axis_tready=m_axis_tready,
+        m_axis_tlast=m_axis_tlast,
+        m_axis_tid=m_axis_tid,
+        m_axis_tdest=m_axis_tdest,
+        m_axis_tuser=m_axis_tuser,
 
         enable=enable,
+        drop=drop,
         select=select
     )
 
@@ -271,6 +190,7 @@ def bench():
 
         yield clk.posedge
         enable.next = True
+        drop.next = False
 
         yield clk.posedge
         print("test 1: select port 0")
@@ -289,8 +209,8 @@ def bench():
 
         source.send(test_frame)
 
-        yield sink_0.wait()
-        rx_frame = sink_0.recv()
+        yield sink_list[0].wait()
+        rx_frame = sink_list[0].recv()
 
         assert rx_frame == test_frame
 
@@ -313,8 +233,8 @@ def bench():
 
         source.send(test_frame)
 
-        yield sink_1.wait()
-        rx_frame = sink_1.recv()
+        yield sink_list[1].wait()
+        rx_frame = sink_list[1].recv()
 
         assert rx_frame == test_frame
 
@@ -346,13 +266,13 @@ def bench():
         source.send(test_frame1)
         source.send(test_frame2)
 
-        yield sink_0.wait()
-        rx_frame = sink_0.recv()
+        yield sink_list[0].wait()
+        rx_frame = sink_list[0].recv()
 
         assert rx_frame == test_frame1
 
-        yield sink_0.wait()
-        rx_frame = sink_0.recv()
+        yield sink_list[0].wait()
+        rx_frame = sink_list[0].recv()
 
         assert rx_frame == test_frame2
 
@@ -385,17 +305,17 @@ def bench():
         source.send(test_frame2)
         yield clk.posedge
 
-        while input_axis_tvalid:
+        while s_axis_tvalid:
             yield clk.posedge
             select.next = 2
 
-        yield sink_1.wait()
-        rx_frame = sink_1.recv()
+        yield sink_list[1].wait()
+        rx_frame = sink_list[1].recv()
 
         assert rx_frame == test_frame1
 
-        yield sink_2.wait()
-        rx_frame = sink_2.recv()
+        yield sink_list[2].wait()
+        rx_frame = sink_list[2].recv()
 
         assert rx_frame == test_frame2
 
@@ -428,7 +348,7 @@ def bench():
         source.send(test_frame2)
         yield clk.posedge
 
-        while input_axis_tvalid:
+        while s_axis_tvalid:
             source_pause.next = True
             yield clk.posedge
             yield clk.posedge
@@ -437,13 +357,13 @@ def bench():
             yield clk.posedge
             select.next = 2
 
-        yield sink_1.wait()
-        rx_frame = sink_1.recv()
+        yield sink_list[1].wait()
+        rx_frame = sink_list[1].recv()
 
         assert rx_frame == test_frame1
 
-        yield sink_2.wait()
-        rx_frame = sink_2.recv()
+        yield sink_list[2].wait()
+        rx_frame = sink_list[2].recv()
 
         assert rx_frame == test_frame2
 
@@ -476,30 +396,87 @@ def bench():
         source.send(test_frame2)
         yield clk.posedge
 
-        while input_axis_tvalid:
-            sink_0_pause.next = True
-            sink_1_pause.next = True
-            sink_2_pause.next = True
-            sink_3_pause.next = True
+        while s_axis_tvalid:
+            sink_pause_list[0].next = True
+            sink_pause_list[1].next = True
+            sink_pause_list[2].next = True
+            sink_pause_list[3].next = True
             yield clk.posedge
             yield clk.posedge
             yield clk.posedge
-            sink_0_pause.next = False
-            sink_1_pause.next = False
-            sink_2_pause.next = False
-            sink_3_pause.next = False
+            sink_pause_list[0].next = False
+            sink_pause_list[1].next = False
+            sink_pause_list[2].next = False
+            sink_pause_list[3].next = False
             yield clk.posedge
             select.next = 2
 
-        yield sink_1.wait()
-        rx_frame = sink_1.recv()
+        yield sink_list[1].wait()
+        rx_frame = sink_list[1].recv()
 
         assert rx_frame == test_frame1
 
-        yield sink_2.wait()
-        rx_frame = sink_2.recv()
+        yield sink_list[2].wait()
+        rx_frame = sink_list[2].recv()
 
         assert rx_frame == test_frame2
+
+        yield delay(100)
+
+        yield clk.posedge
+        print("test 7: enable")
+        current_test.next = 7
+
+        enable.next = False
+        select.next = 0
+
+        test_frame = axis_ep.AXIStreamFrame(
+            b'\xDA\xD1\xD2\xD3\xD4\xD5' +
+            b'\x5A\x51\x52\x53\x54\x55' +
+            b'\x80\x00' +
+            b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10',
+            id=7,
+            dest=1
+        )
+
+        source.send(test_frame)
+
+        yield delay(100)
+
+        assert sink_list[0].empty()
+
+        enable.next = True
+
+        yield sink_list[0].wait()
+        rx_frame = sink_list[0].recv()
+
+        assert rx_frame == test_frame
+
+        yield delay(100)
+
+        yield clk.posedge
+        print("test 8: drop")
+        current_test.next = 8
+
+        drop.next = True
+        select.next = 0
+
+        test_frame = axis_ep.AXIStreamFrame(
+            b'\xDA\xD1\xD2\xD3\xD4\xD5' +
+            b'\x5A\x51\x52\x53\x54\x55' +
+            b'\x80\x00' +
+            b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10',
+            id=8,
+            dest=1
+        )
+
+        source.send(test_frame)
+
+        yield delay(100)
+
+        assert sink_list[0].empty()
+
+        drop.next = False
 
         yield delay(100)
 
--- tb/test_axis_demux_4_64.v
+++ tb/test_axis_demux_4_64.v
@@ -27,11 +27,12 @@ THE SOFTWARE.
 `timescale 1ns / 1ps
 
 /*
- * Testbench for axis_demux_4
+ * Testbench for axis_demux
  */
 module test_axis_demux_4_64;
 
 // Parameters
+parameter M_COUNT = 4;
 parameter DATA_WIDTH = 64;
 parameter KEEP_ENABLE = (DATA_WIDTH>8);
 parameter KEEP_WIDTH = (DATA_WIDTH/8);
@@ -47,53 +48,30 @@ reg clk = 0;
 reg rst = 0;
 reg [7:0] current_test = 0;
 
-reg [DATA_WIDTH-1:0] input_axis_tdata = 0;
-reg [KEEP_WIDTH-1:0] input_axis_tkeep = 0;
-reg input_axis_tvalid = 0;
-reg input_axis_tlast = 0;
-reg [ID_WIDTH-1:0] input_axis_tid = 0;
-reg [DEST_WIDTH-1:0] input_axis_tdest = 0;
-reg [USER_WIDTH-1:0] input_axis_tuser = 0;
+reg [DATA_WIDTH-1:0] s_axis_tdata = 0;
+reg [KEEP_WIDTH-1:0] s_axis_tkeep = 0;
+reg s_axis_tvalid = 0;
+reg s_axis_tlast = 0;
+reg [ID_WIDTH-1:0] s_axis_tid = 0;
+reg [DEST_WIDTH-1:0] s_axis_tdest = 0;
+reg [USER_WIDTH-1:0] s_axis_tuser = 0;
 
-reg output_0_axis_tready = 0;
-reg output_1_axis_tready = 0;
-reg output_2_axis_tready = 0;
-reg output_3_axis_tready = 0;
+reg [M_COUNT-1:0] m_axis_tready = 0;
 
 reg enable = 0;
-reg [1:0] select = 0;
+reg drop = 0;
+reg [$clog2(M_COUNT)-1:0] select = 0;
 
 // Outputs
-wire input_axis_tready;
+wire s_axis_tready;
 
-wire [DATA_WIDTH-1:0] output_0_axis_tdata;
-wire [KEEP_WIDTH-1:0] output_0_axis_tkeep;
-wire output_0_axis_tvalid;
-wire output_0_axis_tlast;
-wire [ID_WIDTH-1:0] output_0_axis_tid;
-wire [DEST_WIDTH-1:0] output_0_axis_tdest;
-wire [USER_WIDTH-1:0] output_0_axis_tuser;
-wire [DATA_WIDTH-1:0] output_1_axis_tdata;
-wire [KEEP_WIDTH-1:0] output_1_axis_tkeep;
-wire output_1_axis_tvalid;
-wire output_1_axis_tlast;
-wire [ID_WIDTH-1:0] output_1_axis_tid;
-wire [DEST_WIDTH-1:0] output_1_axis_tdest;
-wire [USER_WIDTH-1:0] output_1_axis_tuser;
-wire [DATA_WIDTH-1:0] output_2_axis_tdata;
-wire [KEEP_WIDTH-1:0] output_2_axis_tkeep;
-wire output_2_axis_tvalid;
-wire output_2_axis_tlast;
-wire [ID_WIDTH-1:0] output_2_axis_tid;
-wire [DEST_WIDTH-1:0] output_2_axis_tdest;
-wire [USER_WIDTH-1:0] output_2_axis_tuser;
-wire [DATA_WIDTH-1:0] output_3_axis_tdata;
-wire [KEEP_WIDTH-1:0] output_3_axis_tkeep;
-wire output_3_axis_tvalid;
-wire output_3_axis_tlast;
-wire [ID_WIDTH-1:0] output_3_axis_tid;
-wire [DEST_WIDTH-1:0] output_3_axis_tdest;
-wire [USER_WIDTH-1:0] output_3_axis_tuser;
+wire [M_COUNT*DATA_WIDTH-1:0] m_axis_tdata;
+wire [M_COUNT*KEEP_WIDTH-1:0] m_axis_tkeep;
+wire [M_COUNT-1:0] m_axis_tvalid;
+wire [M_COUNT-1:0] m_axis_tlast;
+wire [M_COUNT*ID_WIDTH-1:0] m_axis_tid;
+wire [M_COUNT*DEST_WIDTH-1:0] m_axis_tdest;
+wire [M_COUNT*USER_WIDTH-1:0] m_axis_tuser;
 
 initial begin
     // myhdl integration
@@ -101,50 +79,27 @@ initial begin
         clk,
         rst,
         current_test,
-        input_axis_tdata,
-        input_axis_tkeep,
-        input_axis_tvalid,
-        input_axis_tlast,
-        input_axis_tid,
-        input_axis_tdest,
-        input_axis_tuser,
-        output_0_axis_tready,
-        output_1_axis_tready,
-        output_2_axis_tready,
-        output_3_axis_tready,
+        s_axis_tdata,
+        s_axis_tkeep,
+        s_axis_tvalid,
+        s_axis_tlast,
+        s_axis_tid,
+        s_axis_tdest,
+        s_axis_tuser,
+        m_axis_tready,
         enable,
+        drop,
         select
     );
     $to_myhdl(
-        input_axis_tready,
-        output_0_axis_tdata,
-        output_0_axis_tkeep,
-        output_0_axis_tvalid,
-        output_0_axis_tlast,
-        output_0_axis_tid,
-        output_0_axis_tdest,
-        output_0_axis_tuser,
-        output_1_axis_tdata,
-        output_1_axis_tkeep,
-        output_1_axis_tvalid,
-        output_1_axis_tlast,
-        output_1_axis_tid,
-        output_1_axis_tdest,
-        output_1_axis_tuser,
-        output_2_axis_tdata,
-        output_2_axis_tkeep,
-        output_2_axis_tvalid,
-        output_2_axis_tlast,
-        output_2_axis_tid,
-        output_2_axis_tdest,
-        output_2_axis_tuser,
-        output_3_axis_tdata,
-        output_3_axis_tkeep,
-        output_3_axis_tvalid,
-        output_3_axis_tlast,
-        output_3_axis_tid,
-        output_3_axis_tdest,
-        output_3_axis_tuser
+        s_axis_tready,
+        m_axis_tdata,
+        m_axis_tkeep,
+        m_axis_tvalid,
+        m_axis_tlast,
+        m_axis_tid,
+        m_axis_tdest,
+        m_axis_tuser
     );
 
     // dump file
@@ -152,7 +107,8 @@ initial begin
     $dumpvars(0, test_axis_demux_4_64);
 end
 
-axis_demux_4 #(
+axis_demux #(
+    .M_COUNT(M_COUNT),
     .DATA_WIDTH(DATA_WIDTH),
     .KEEP_ENABLE(KEEP_ENABLE),
     .KEEP_WIDTH(KEEP_WIDTH),
@@ -167,49 +123,26 @@ UUT (
     .clk(clk),
     .rst(rst),
     // AXI input
-    .input_axis_tdata(input_axis_tdata),
-    .input_axis_tkeep(input_axis_tkeep),
-    .input_axis_tvalid(input_axis_tvalid),
-    .input_axis_tready(input_axis_tready),
-    .input_axis_tlast(input_axis_tlast),
-    .input_axis_tid(input_axis_tid),
-    .input_axis_tdest(input_axis_tdest),
-    .input_axis_tuser(input_axis_tuser),
+    .s_axis_tdata(s_axis_tdata),
+    .s_axis_tkeep(s_axis_tkeep),
+    .s_axis_tvalid(s_axis_tvalid),
+    .s_axis_tready(s_axis_tready),
+    .s_axis_tlast(s_axis_tlast),
+    .s_axis_tid(s_axis_tid),
+    .s_axis_tdest(s_axis_tdest),
+    .s_axis_tuser(s_axis_tuser),
     // AXI outputs
-    .output_0_axis_tdata(output_0_axis_tdata),
-    .output_0_axis_tkeep(output_0_axis_tkeep),
-    .output_0_axis_tvalid(output_0_axis_tvalid),
-    .output_0_axis_tready(output_0_axis_tready),
-    .output_0_axis_tlast(output_0_axis_tlast),
-    .output_0_axis_tid(output_0_axis_tid),
-    .output_0_axis_tdest(output_0_axis_tdest),
-    .output_0_axis_tuser(output_0_axis_tuser),
-    .output_1_axis_tdata(output_1_axis_tdata),
-    .output_1_axis_tkeep(output_1_axis_tkeep),
-    .output_1_axis_tvalid(output_1_axis_tvalid),
-    .output_1_axis_tready(output_1_axis_tready),
-    .output_1_axis_tlast(output_1_axis_tlast),
-    .output_1_axis_tid(output_1_axis_tid),
-    .output_1_axis_tdest(output_1_axis_tdest),
-    .output_1_axis_tuser(output_1_axis_tuser),
-    .output_2_axis_tdata(output_2_axis_tdata),
-    .output_2_axis_tkeep(output_2_axis_tkeep),
-    .output_2_axis_tvalid(output_2_axis_tvalid),
-    .output_2_axis_tready(output_2_axis_tready),
-    .output_2_axis_tlast(output_2_axis_tlast),
-    .output_2_axis_tid(output_2_axis_tid),
-    .output_2_axis_tdest(output_2_axis_tdest),
-    .output_2_axis_tuser(output_2_axis_tuser),
-    .output_3_axis_tdata(output_3_axis_tdata),
-    .output_3_axis_tkeep(output_3_axis_tkeep),
-    .output_3_axis_tvalid(output_3_axis_tvalid),
-    .output_3_axis_tready(output_3_axis_tready),
-    .output_3_axis_tlast(output_3_axis_tlast),
-    .output_3_axis_tid(output_3_axis_tid),
-    .output_3_axis_tdest(output_3_axis_tdest),
-    .output_3_axis_tuser(output_3_axis_tuser),
+    .m_axis_tdata(m_axis_tdata),
+    .m_axis_tkeep(m_axis_tkeep),
+    .m_axis_tvalid(m_axis_tvalid),
+    .m_axis_tready(m_axis_tready),
+    .m_axis_tlast(m_axis_tlast),
+    .m_axis_tid(m_axis_tid),
+    .m_axis_tdest(m_axis_tdest),
+    .m_axis_tuser(m_axis_tuser),
     // Control
     .enable(enable),
+    .drop(drop),
     .select(select)
 );
 
