6a4bcaab38411ed30ee6111d8ab2defa9717c55a
Alex Forencich <alex@alexforencich.com>
2020-09-22 22:13:59 -0700

    Add timing constraints for LED driver

---
+++ example/fb2CG/fpga_10g/led.tcl
@@ -0,0 +1,12 @@
+# Timing constraints for led_sreg_driver
+
+foreach inst [get_cells -hier -filter {(ORIG_REF_NAME == led_sreg_driver || REF_NAME == led_sreg_driver)}] {
+    puts "Inserting timing constraints for led_sreg_driver instance $inst"
+
+    set select_ffs [get_cells "$inst/led_sync_reg_1_reg[*] $inst/led_sync_reg_2_reg[*]"]
+
+    if {[llength $select_ffs]} {
+        set_property ASYNC_REG TRUE $select_ffs
+        set_false_path -from [all_registers] -to [get_cells "$inst/led_sync_reg_1_reg[*]"]
+    }
+}
