e3f8879474e4c3a49acdea74bf2b725f4fe8ea47
Alex Forencich <alex@alexforencich.com>
2021-10-19 18:30:35 -0700

    Rework GT instances in ZCU106 design

--- example/ZCU106/fpga/rtl/fpga_core.v
+++ example/ZCU106/fpga/rtl/fpga_core.v
@@ -1,6 +1,6 @@
 /*
 
-Copyright (c) 2020 Alex Forencich
+Copyright (c) 2020-2021 Alex Forencich
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
