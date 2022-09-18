b9e0af36341a81ae80d6a8467b029d79c69b71cb
Alex Forencich <alex@alexforencich.com>
2022-09-18 12:07:11 -0700

    Revert change to early ready conditions for improved throughput
    Signed-off-by: Alex Forencich <alex@alexforencich.com>

--- rtl/axis_cobs_decode.v
+++ rtl/axis_cobs_decode.v
@@ -268,8 +268,8 @@ assign m_axis_tvalid = m_axis_tvalid_reg;
 assign m_axis_tlast = m_axis_tlast_reg;
 assign m_axis_tuser = m_axis_tuser_reg;
 
-// enable ready input next cycle if output is ready or if both output registers are empty
-assign m_axis_tready_int_early = m_axis_tready || (!temp_m_axis_tvalid_reg && !m_axis_tvalid_reg);
+// enable ready input next cycle if output is ready or the temp reg will not be filled on the next cycle (output reg empty or no input)
+assign m_axis_tready_int_early = m_axis_tready || (!temp_m_axis_tvalid_reg && (!m_axis_tvalid_reg || !m_axis_tvalid_int));
 
 always @* begin
     // transfer sink ready state to source
