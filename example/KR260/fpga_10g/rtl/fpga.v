9212b89e932c1a79ba361d03ad955d9136b0df7f
Víctor Mayoral Vilches <v.mayoralv@gmail.com>
2023-04-02 00:17:37 +0200

    Comment out additions, validate that starting point builds
    Signed-off-by: Víctor Mayoral Vilches <v.mayoralv@gmail.com>

--- example/KR260/fpga_10g/rtl/fpga.v
+++ example/KR260/fpga_10g/rtl/fpga.v
@@ -32,29 +32,29 @@ THE SOFTWARE.
 /*
  * FPGA top-level module
  */
-module fpga #
-(
-    // AXI interface configuration (DMA)
-    parameter AXI_DATA_WIDTH = 128,
-    parameter AXI_ADDR_WIDTH = 32,
-    parameter AXI_STRB_WIDTH = (AXI_DATA_WIDTH/8),
-    parameter AXI_ID_WIDTH = 8,
+// module fpga #
+// (
+//     // AXI interface configuration (DMA)
+//     parameter AXI_DATA_WIDTH = 128,
+//     parameter AXI_ADDR_WIDTH = 32,
+//     parameter AXI_STRB_WIDTH = (AXI_DATA_WIDTH/8),
+//     parameter AXI_ID_WIDTH = 8,
 
-    // Interrupts
-    parameter IRQ_COUNT = 32,
-    parameter IRQ_STRETCH = 10,
+//     // Interrupts
+//     parameter IRQ_COUNT = 32,
+//     parameter IRQ_STRETCH = 10,
 
-    // AXI lite interface configuration (control)
-    parameter AXIL_CTRL_DATA_WIDTH = 32,
-    parameter AXIL_CTRL_ADDR_WIDTH = 24,
-    parameter AXIL_CTRL_STRB_WIDTH = (AXIL_CTRL_DATA_WIDTH/8),
+//     // AXI lite interface configuration (control)
+//     parameter AXIL_CTRL_DATA_WIDTH = 32,
+//     parameter AXIL_CTRL_ADDR_WIDTH = 24,
+//     parameter AXIL_CTRL_STRB_WIDTH = (AXIL_CTRL_DATA_WIDTH/8),
 
-    // AXI lite interface configuration (application control)
-    parameter AXIL_APP_CTRL_DATA_WIDTH = AXIL_CTRL_DATA_WIDTH,
-    parameter AXIL_APP_CTRL_ADDR_WIDTH = 24,
-    parameter AXIL_APP_CTRL_STRB_WIDTH = (AXIL_APP_CTRL_DATA_WIDTH/8),
-)
-(
+//     // AXI lite interface configuration (application control)
+//     parameter AXIL_APP_CTRL_DATA_WIDTH = AXIL_CTRL_DATA_WIDTH,
+//     parameter AXIL_APP_CTRL_ADDR_WIDTH = 24,
+//     parameter AXIL_APP_CTRL_STRB_WIDTH = (AXIL_APP_CTRL_DATA_WIDTH/8)
+// )
+module fpga (
     /*
      * Clock: 25 MHz LVCMOS18
      */
@@ -190,200 +190,200 @@ sync_reset_125mhz_inst (
     .out(rst_125mhz_int)
 );
 
-// Clock and reset
-wire zynq_pl_clk;
-wire zynq_pl_reset;
+// // Clock and reset
+// wire zynq_pl_clk;
+// wire zynq_pl_reset;
 
-// Zynq AXI MM
-wire [IRQ_COUNT-1:0]                 irq;
+// // Zynq AXI MM
+// wire [IRQ_COUNT-1:0]                 irq;
 
-wire [AXI_ID_WIDTH-1:0]              axi_awid;
-wire [AXI_ADDR_WIDTH-1:0]            axi_awaddr;
-wire [7:0]                           axi_awlen;
-wire [2:0]                           axi_awsize;
-wire [1:0]                           axi_awburst;
-wire                                 axi_awlock;
-wire [3:0]                           axi_awcache;
-wire [2:0]                           axi_awprot;
-wire                                 axi_awvalid;
-wire                                 axi_awready;
-wire [AXI_DATA_WIDTH-1:0]            axi_wdata;
-wire [AXI_STRB_WIDTH-1:0]            axi_wstrb;
-wire                                 axi_wlast;
-wire                                 axi_wvalid;
-wire                                 axi_wready;
-wire [AXI_ID_WIDTH-1:0]              axi_bid;
-wire [1:0]                           axi_bresp;
-wire                                 axi_bvalid;
-wire                                 axi_bready;
-wire [AXI_ID_WIDTH-1:0]              axi_arid;
-wire [AXI_ADDR_WIDTH-1:0]            axi_araddr;
-wire [7:0]                           axi_arlen;
-wire [2:0]                           axi_arsize;
-wire [1:0]                           axi_arburst;
-wire                                 axi_arlock;
-wire [3:0]                           axi_arcache;
-wire [2:0]                           axi_arprot;
-wire                                 axi_arvalid;
-wire                                 axi_arready;
-wire [AXI_ID_WIDTH-1:0]              axi_rid;
-wire [AXI_DATA_WIDTH-1:0]            axi_rdata;
-wire [1:0]                           axi_rresp;
-wire                                 axi_rlast;
-wire                                 axi_rvalid;
-wire                                 axi_rready;
+// wire [AXI_ID_WIDTH-1:0]              axi_awid;
+// wire [AXI_ADDR_WIDTH-1:0]            axi_awaddr;
+// wire [7:0]                           axi_awlen;
+// wire [2:0]                           axi_awsize;
+// wire [1:0]                           axi_awburst;
+// wire                                 axi_awlock;
+// wire [3:0]                           axi_awcache;
+// wire [2:0]                           axi_awprot;
+// wire                                 axi_awvalid;
+// wire                                 axi_awready;
+// wire [AXI_DATA_WIDTH-1:0]            axi_wdata;
+// wire [AXI_STRB_WIDTH-1:0]            axi_wstrb;
+// wire                                 axi_wlast;
+// wire                                 axi_wvalid;
+// wire                                 axi_wready;
+// wire [AXI_ID_WIDTH-1:0]              axi_bid;
+// wire [1:0]                           axi_bresp;
+// wire                                 axi_bvalid;
+// wire                                 axi_bready;
+// wire [AXI_ID_WIDTH-1:0]              axi_arid;
+// wire [AXI_ADDR_WIDTH-1:0]            axi_araddr;
+// wire [7:0]                           axi_arlen;
+// wire [2:0]                           axi_arsize;
+// wire [1:0]                           axi_arburst;
+// wire                                 axi_arlock;
+// wire [3:0]                           axi_arcache;
+// wire [2:0]                           axi_arprot;
+// wire                                 axi_arvalid;
+// wire                                 axi_arready;
+// wire [AXI_ID_WIDTH-1:0]              axi_rid;
+// wire [AXI_DATA_WIDTH-1:0]            axi_rdata;
+// wire [1:0]                           axi_rresp;
+// wire                                 axi_rlast;
+// wire                                 axi_rvalid;
+// wire                                 axi_rready;
 
-// AXI lite connections
-wire [AXIL_CTRL_ADDR_WIDTH-1:0]      axil_ctrl_awaddr;
-wire [2:0]                           axil_ctrl_awprot;
-wire                                 axil_ctrl_awvalid;
-wire                                 axil_ctrl_awready;
-wire [AXIL_CTRL_DATA_WIDTH-1:0]      axil_ctrl_wdata;
-wire [AXIL_CTRL_STRB_WIDTH-1:0]      axil_ctrl_wstrb;
-wire                                 axil_ctrl_wvalid;
-wire                                 axil_ctrl_wready;
-wire [1:0]                           axil_ctrl_bresp;
-wire                                 axil_ctrl_bvalid;
-wire                                 axil_ctrl_bready;
-wire [AXIL_CTRL_ADDR_WIDTH-1:0]      axil_ctrl_araddr;
-wire [2:0]                           axil_ctrl_arprot;
-wire                                 axil_ctrl_arvalid;
-wire                                 axil_ctrl_arready;
-wire [AXIL_CTRL_DATA_WIDTH-1:0]      axil_ctrl_rdata;
-wire [1:0]                           axil_ctrl_rresp;
-wire                                 axil_ctrl_rvalid;
-wire                                 axil_ctrl_rready;
+// // AXI lite connections
+// wire [AXIL_CTRL_ADDR_WIDTH-1:0]      axil_ctrl_awaddr;
+// wire [2:0]                           axil_ctrl_awprot;
+// wire                                 axil_ctrl_awvalid;
+// wire                                 axil_ctrl_awready;
+// wire [AXIL_CTRL_DATA_WIDTH-1:0]      axil_ctrl_wdata;
+// wire [AXIL_CTRL_STRB_WIDTH-1:0]      axil_ctrl_wstrb;
+// wire                                 axil_ctrl_wvalid;
+// wire                                 axil_ctrl_wready;
+// wire [1:0]                           axil_ctrl_bresp;
+// wire                                 axil_ctrl_bvalid;
+// wire                                 axil_ctrl_bready;
+// wire [AXIL_CTRL_ADDR_WIDTH-1:0]      axil_ctrl_araddr;
+// wire [2:0]                           axil_ctrl_arprot;
+// wire                                 axil_ctrl_arvalid;
+// wire                                 axil_ctrl_arready;
+// wire [AXIL_CTRL_DATA_WIDTH-1:0]      axil_ctrl_rdata;
+// wire [1:0]                           axil_ctrl_rresp;
+// wire                                 axil_ctrl_rvalid;
+// wire                                 axil_ctrl_rready;
 
-wire [AXIL_APP_CTRL_ADDR_WIDTH-1:0]  axil_app_ctrl_awaddr;
-wire [2:0]                           axil_app_ctrl_awprot;
-wire                                 axil_app_ctrl_awvalid;
-wire                                 axil_app_ctrl_awready;
-wire [AXIL_APP_CTRL_DATA_WIDTH-1:0]  axil_app_ctrl_wdata;
-wire [AXIL_APP_CTRL_STRB_WIDTH-1:0]  axil_app_ctrl_wstrb;
-wire                                 axil_app_ctrl_wvalid;
-wire                                 axil_app_ctrl_wready;
-wire [1:0]                           axil_app_ctrl_bresp;
-wire                                 axil_app_ctrl_bvalid;
-wire                                 axil_app_ctrl_bready;
-wire [AXIL_APP_CTRL_ADDR_WIDTH-1:0]  axil_app_ctrl_araddr;
-wire [2:0]                           axil_app_ctrl_arprot;
-wire                                 axil_app_ctrl_arvalid;
-wire                                 axil_app_ctrl_arready;
-wire [AXIL_APP_CTRL_DATA_WIDTH-1:0]  axil_app_ctrl_rdata;
-wire [1:0]                           axil_app_ctrl_rresp;
-wire                                 axil_app_ctrl_rvalid;
-wire                                 axil_app_ctrl_rready;
+// wire [AXIL_APP_CTRL_ADDR_WIDTH-1:0]  axil_app_ctrl_awaddr;
+// wire [2:0]                           axil_app_ctrl_awprot;
+// wire                                 axil_app_ctrl_awvalid;
+// wire                                 axil_app_ctrl_awready;
+// wire [AXIL_APP_CTRL_DATA_WIDTH-1:0]  axil_app_ctrl_wdata;
+// wire [AXIL_APP_CTRL_STRB_WIDTH-1:0]  axil_app_ctrl_wstrb;
+// wire                                 axil_app_ctrl_wvalid;
+// wire                                 axil_app_ctrl_wready;
+// wire [1:0]                           axil_app_ctrl_bresp;
+// wire                                 axil_app_ctrl_bvalid;
+// wire                                 axil_app_ctrl_bready;
+// wire [AXIL_APP_CTRL_ADDR_WIDTH-1:0]  axil_app_ctrl_araddr;
+// wire [2:0]                           axil_app_ctrl_arprot;
+// wire                                 axil_app_ctrl_arvalid;
+// wire                                 axil_app_ctrl_arready;
+// wire [AXIL_APP_CTRL_DATA_WIDTH-1:0]  axil_app_ctrl_rdata;
+// wire [1:0]                           axil_app_ctrl_rresp;
+// wire                                 axil_app_ctrl_rvalid;
+// wire                                 axil_app_ctrl_rready;
 
-// implements an interrupt stretching mechanism to extend the duration 
-// of an interrupt signal to ensure that it is long enough to be detected 
-// by the receiving system. 
-reg [(IRQ_COUNT*IRQ_STRETCH)-1:0] irq_stretch = {(IRQ_COUNT*IRQ_STRETCH){1'b0}};
-always @(posedge zynq_pl_clk) begin
-    if (zynq_pl_reset) begin
-        irq_stretch <= {(IRQ_COUNT*IRQ_STRETCH){1'b0}};
-    end else begin
-        /* IRQ shift vector */
-        irq_stretch <= {irq_stretch[0 +: (IRQ_COUNT*IRQ_STRETCH)-IRQ_COUNT], irq};
-    end
-end
+// // implements an interrupt stretching mechanism to extend the duration 
+// // of an interrupt signal to ensure that it is long enough to be detected 
+// // by the receiving system. 
+// reg [(IRQ_COUNT*IRQ_STRETCH)-1:0] irq_stretch = {(IRQ_COUNT*IRQ_STRETCH){1'b0}};
+// always @(posedge zynq_pl_clk) begin
+//     if (zynq_pl_reset) begin
+//         irq_stretch <= {(IRQ_COUNT*IRQ_STRETCH){1'b0}};
+//     end else begin
+//         /* IRQ shift vector */
+//         irq_stretch <= {irq_stretch[0 +: (IRQ_COUNT*IRQ_STRETCH)-IRQ_COUNT], irq};
+//     end
+// end
 
-reg [IRQ_COUNT-1:0] zynq_irq;
-integer i, k;
-always @* begin
-    for (k = 0; k < IRQ_COUNT; k = k + 1) begin
-        zynq_irq[k] = 1'b0;
-        for (i = 0; i < (IRQ_COUNT*IRQ_STRETCH); i = i + IRQ_COUNT) begin
-            zynq_irq[k] = zynq_irq[k] | irq_stretch[k + i];
-        end
-    end
-end
+// reg [IRQ_COUNT-1:0] zynq_irq;
+// integer i, k;
+// always @* begin
+//     for (k = 0; k < IRQ_COUNT; k = k + 1) begin
+//         zynq_irq[k] = 1'b0;
+//         for (i = 0; i < (IRQ_COUNT*IRQ_STRETCH); i = i + IRQ_COUNT) begin
+//             zynq_irq[k] = zynq_irq[k] | irq_stretch[k + i];
+//         end
+//     end
+// end
 
-// Zynq UltraScale+ PS
-zynq_ps zynq_ps_inst (
-    .pl_clk0(zynq_pl_clk),
-    .pl_reset(zynq_pl_reset),
-    .pl_ps_irq0(zynq_irq),
+// // Zynq UltraScale+ PS
+// zynq_ps zynq_ps_inst (
+//     .pl_clk0(zynq_pl_clk),
+//     .pl_reset(zynq_pl_reset),
+//     .pl_ps_irq0(zynq_irq),
 
-    .m_axil_ctrl_araddr(axil_ctrl_araddr),
-    .m_axil_ctrl_arprot(axil_ctrl_arprot),
-    .m_axil_ctrl_arready(axil_ctrl_arready),
-    .m_axil_ctrl_arvalid(axil_ctrl_arvalid),
-    .m_axil_ctrl_awaddr(axil_ctrl_awaddr),
-    .m_axil_ctrl_awprot(axil_ctrl_awprot),
-    .m_axil_ctrl_awready(axil_ctrl_awready),
-    .m_axil_ctrl_awvalid(axil_ctrl_awvalid),
-    .m_axil_ctrl_bready(axil_ctrl_bready),
-    .m_axil_ctrl_bresp(axil_ctrl_bresp),
-    .m_axil_ctrl_bvalid(axil_ctrl_bvalid),
-    .m_axil_ctrl_rdata(axil_ctrl_rdata),
-    .m_axil_ctrl_rready(axil_ctrl_rready),
-    .m_axil_ctrl_rresp(axil_ctrl_rresp),
-    .m_axil_ctrl_rvalid(axil_ctrl_rvalid),
-    .m_axil_ctrl_wdata(axil_ctrl_wdata),
-    .m_axil_ctrl_wready(axil_ctrl_wready),
-    .m_axil_ctrl_wstrb(axil_ctrl_wstrb),
-    .m_axil_ctrl_wvalid(axil_ctrl_wvalid),
+//     .m_axil_ctrl_araddr(axil_ctrl_araddr),
+//     .m_axil_ctrl_arprot(axil_ctrl_arprot),
+//     .m_axil_ctrl_arready(axil_ctrl_arready),
+//     .m_axil_ctrl_arvalid(axil_ctrl_arvalid),
+//     .m_axil_ctrl_awaddr(axil_ctrl_awaddr),
+//     .m_axil_ctrl_awprot(axil_ctrl_awprot),
+//     .m_axil_ctrl_awready(axil_ctrl_awready),
+//     .m_axil_ctrl_awvalid(axil_ctrl_awvalid),
+//     .m_axil_ctrl_bready(axil_ctrl_bready),
+//     .m_axil_ctrl_bresp(axil_ctrl_bresp),
+//     .m_axil_ctrl_bvalid(axil_ctrl_bvalid),
+//     .m_axil_ctrl_rdata(axil_ctrl_rdata),
+//     .m_axil_ctrl_rready(axil_ctrl_rready),
+//     .m_axil_ctrl_rresp(axil_ctrl_rresp),
+//     .m_axil_ctrl_rvalid(axil_ctrl_rvalid),
+//     .m_axil_ctrl_wdata(axil_ctrl_wdata),
+//     .m_axil_ctrl_wready(axil_ctrl_wready),
+//     .m_axil_ctrl_wstrb(axil_ctrl_wstrb),
+//     .m_axil_ctrl_wvalid(axil_ctrl_wvalid),
 
-    .m_axil_app_ctrl_araddr(axil_app_ctrl_araddr),
-    .m_axil_app_ctrl_arprot(axil_app_ctrl_arprot),
-    .m_axil_app_ctrl_arready(axil_app_ctrl_arready),
-    .m_axil_app_ctrl_arvalid(axil_app_ctrl_arvalid),
-    .m_axil_app_ctrl_awaddr(axil_app_ctrl_awaddr),
-    .m_axil_app_ctrl_awprot(axil_app_ctrl_awprot),
-    .m_axil_app_ctrl_awready(axil_app_ctrl_awready),
-    .m_axil_app_ctrl_awvalid(axil_app_ctrl_awvalid),
-    .m_axil_app_ctrl_bready(axil_app_ctrl_bready),
-    .m_axil_app_ctrl_bresp(axil_app_ctrl_bresp),
-    .m_axil_app_ctrl_bvalid(axil_app_ctrl_bvalid),
-    .m_axil_app_ctrl_rdata(axil_app_ctrl_rdata),
-    .m_axil_app_ctrl_rready(axil_app_ctrl_rready),
-    .m_axil_app_ctrl_rresp(axil_app_ctrl_rresp),
-    .m_axil_app_ctrl_rvalid(axil_app_ctrl_rvalid),
-    .m_axil_app_ctrl_wdata(axil_app_ctrl_wdata),
-    .m_axil_app_ctrl_wready(axil_app_ctrl_wready),
-    .m_axil_app_ctrl_wstrb(axil_app_ctrl_wstrb),
-    .m_axil_app_ctrl_wvalid(axil_app_ctrl_wvalid),
+//     .m_axil_app_ctrl_araddr(axil_app_ctrl_araddr),
+//     .m_axil_app_ctrl_arprot(axil_app_ctrl_arprot),
+//     .m_axil_app_ctrl_arready(axil_app_ctrl_arready),
+//     .m_axil_app_ctrl_arvalid(axil_app_ctrl_arvalid),
+//     .m_axil_app_ctrl_awaddr(axil_app_ctrl_awaddr),
+//     .m_axil_app_ctrl_awprot(axil_app_ctrl_awprot),
+//     .m_axil_app_ctrl_awready(axil_app_ctrl_awready),
+//     .m_axil_app_ctrl_awvalid(axil_app_ctrl_awvalid),
+//     .m_axil_app_ctrl_bready(axil_app_ctrl_bready),
+//     .m_axil_app_ctrl_bresp(axil_app_ctrl_bresp),
+//     .m_axil_app_ctrl_bvalid(axil_app_ctrl_bvalid),
+//     .m_axil_app_ctrl_rdata(axil_app_ctrl_rdata),
+//     .m_axil_app_ctrl_rready(axil_app_ctrl_rready),
+//     .m_axil_app_ctrl_rresp(axil_app_ctrl_rresp),
+//     .m_axil_app_ctrl_rvalid(axil_app_ctrl_rvalid),
+//     .m_axil_app_ctrl_wdata(axil_app_ctrl_wdata),
+//     .m_axil_app_ctrl_wready(axil_app_ctrl_wready),
+//     .m_axil_app_ctrl_wstrb(axil_app_ctrl_wstrb),
+//     .m_axil_app_ctrl_wvalid(axil_app_ctrl_wvalid),
 
-    .s_axi_dma_araddr(axi_araddr),
-    .s_axi_dma_arburst(axi_arburst),
-    .s_axi_dma_arcache(axi_arcache),
-    .s_axi_dma_arid(axi_arid),
-    .s_axi_dma_arlen(axi_arlen),
-    .s_axi_dma_arlock(axi_arlock),
-    .s_axi_dma_arprot(axi_arprot),
-    .s_axi_dma_arqos({4{1'b0}}),
-    .s_axi_dma_arready(axi_arready),
-    .s_axi_dma_arsize(axi_arsize),
-    .s_axi_dma_aruser(1'b0),
-    .s_axi_dma_arvalid(axi_arvalid),
-    .s_axi_dma_awaddr(axi_awaddr),
-    .s_axi_dma_awburst(axi_awburst),
-    .s_axi_dma_awcache(axi_awcache),
-    .s_axi_dma_awid(axi_awid),
-    .s_axi_dma_awlen(axi_awlen),
-    .s_axi_dma_awlock(axi_awlock),
-    .s_axi_dma_awprot(axi_awprot),
-    .s_axi_dma_awqos({4{1'b0}}),
-    .s_axi_dma_awready(axi_awready),
-    .s_axi_dma_awsize(axi_awsize),
-    .s_axi_dma_awuser(1'b0),
-    .s_axi_dma_awvalid(axi_awvalid),
-    .s_axi_dma_bid(axi_bid),
-    .s_axi_dma_bready(axi_bready),
-    .s_axi_dma_bresp(axi_bresp),
-    .s_axi_dma_bvalid(axi_bvalid),
-    .s_axi_dma_rdata(axi_rdata),
-    .s_axi_dma_rid(axi_rid),
-    .s_axi_dma_rlast(axi_rlast),
-    .s_axi_dma_rready(axi_rready),
-    .s_axi_dma_rresp(axi_rresp),
-    .s_axi_dma_rvalid(axi_rvalid),
-    .s_axi_dma_wdata(axi_wdata),
-    .s_axi_dma_wlast(axi_wlast),
-    .s_axi_dma_wready(axi_wready),
-    .s_axi_dma_wstrb(axi_wstrb),
-    .s_axi_dma_wvalid(axi_wvalid)
-);
+//     .s_axi_dma_araddr(axi_araddr),
+//     .s_axi_dma_arburst(axi_arburst),
+//     .s_axi_dma_arcache(axi_arcache),
+//     .s_axi_dma_arid(axi_arid),
+//     .s_axi_dma_arlen(axi_arlen),
+//     .s_axi_dma_arlock(axi_arlock),
+//     .s_axi_dma_arprot(axi_arprot),
+//     .s_axi_dma_arqos({4{1'b0}}),
+//     .s_axi_dma_arready(axi_arready),
+//     .s_axi_dma_arsize(axi_arsize),
+//     .s_axi_dma_aruser(1'b0),
+//     .s_axi_dma_arvalid(axi_arvalid),
+//     .s_axi_dma_awaddr(axi_awaddr),
+//     .s_axi_dma_awburst(axi_awburst),
+//     .s_axi_dma_awcache(axi_awcache),
+//     .s_axi_dma_awid(axi_awid),
+//     .s_axi_dma_awlen(axi_awlen),
+//     .s_axi_dma_awlock(axi_awlock),
+//     .s_axi_dma_awprot(axi_awprot),
+//     .s_axi_dma_awqos({4{1'b0}}),
+//     .s_axi_dma_awready(axi_awready),
+//     .s_axi_dma_awsize(axi_awsize),
+//     .s_axi_dma_awuser(1'b0),
+//     .s_axi_dma_awvalid(axi_awvalid),
+//     .s_axi_dma_bid(axi_bid),
+//     .s_axi_dma_bready(axi_bready),
+//     .s_axi_dma_bresp(axi_bresp),
+//     .s_axi_dma_bvalid(axi_bvalid),
+//     .s_axi_dma_rdata(axi_rdata),
+//     .s_axi_dma_rid(axi_rid),
+//     .s_axi_dma_rlast(axi_rlast),
+//     .s_axi_dma_rready(axi_rready),
+//     .s_axi_dma_rresp(axi_rresp),
+//     .s_axi_dma_rvalid(axi_rvalid),
+//     .s_axi_dma_wdata(axi_wdata),
+//     .s_axi_dma_wlast(axi_wlast),
+//     .s_axi_dma_wready(axi_wready),
+//     .s_axi_dma_wstrb(axi_wstrb),
+//     .s_axi_dma_wvalid(axi_wvalid)
+// );
 
 // XGMII 10G PHY
 assign sfp0_tx_disable_b = 1'b1;
